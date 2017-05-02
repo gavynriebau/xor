046c2aee1acb3c4647bbe7ca64dfd3115e842943
Gavyn <gavyn.riebau@gmail.com>
2017-05-02 19:27:47 +0800

    Added support for using string or file as key

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,36 @@
 
 extern crate clap;
 
-use clap::{App, Arg};
+use clap::{App, Arg, ArgMatches};
 use std::io;
+use std::path::Path;
 use std::fs::File;
 use std::io::{Write, Read};
+
+fn get_key_bytes<'a>(matches: &'a ArgMatches<'a>) -> Vec<u8> {
+    let mut key_bytes : Vec<u8> = Vec::new();
+
+    let key = matches.value_of("key").unwrap();
+    if Path::new(key).exists() {
+        // Key is a file, read the contents of the file.
+        File::open(key).unwrap().read_to_end(&mut key_bytes).unwrap();
+    } else {
+        // Key is a string, use the string bytes
+        key_bytes = key.to_string().into_bytes();
+    }
+
+    key_bytes
+}
 
 fn main() {
 
     // Parse arguments and provide help.
     let matches = App::new("xor")
-        .version("1.0.0")
+        .version("1.1.0")
         .about("XORs input against a provided key")
         .author("Gavyn Riebau")
         .arg(Arg::with_name("key")
-             .help("The file containing the key against which input will be XOR'd. This should be larger than the given input data or will need to be repeated to encode the input data.")
+             .help("The file containing the key data, or a provided string, against which input will be XOR'd. This should be larger than the given input data or will need to be repeated to encode the input data.")
              .long("key")
              .short("k")
              .required(true)
@@ -32,10 +48,8 @@ fn main() {
              .required(false))
      .get_matches();
 
-    // Read the entire key into memory
-    let mut key_file = File::open(matches.value_of("key").unwrap()).unwrap();
-    let mut key_bytes : Vec<u8> = Vec::new();
-    let key_len = key_file.read_to_end(&mut key_bytes).unwrap();
+    let key_bytes = get_key_bytes(&matches);
+    let key_len = key_bytes.len();
 
     let stdin = io::stdin();
     let mut key_idx = 0;
