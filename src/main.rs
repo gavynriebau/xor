d4c78c567ba124ef8308ed748f168a05effa8621
Gavyn <gavyn.riebau@gmail.com>
2017-05-21 18:18:51 +0800

    Minor tidyup

--- src/main.rs
+++ src/main.rs
@@ -12,39 +12,6 @@ const ERR_ENCODED_DATA_NOT_UTF8 : &'static str = r#"ERROR: Encoded data isn't pr
 The encoded data couldn't be encoded to valid utf8 and so couldn't be printed to the screen.
 Use the "-o" option to write the output directly to a file instead."#;
 
-fn get_key_bytes<'a>(matches: &'a ArgMatches<'a>) -> Vec<u8> {
-    let mut key_bytes : Vec<u8> = Vec::new();
-
-    let key = matches.value_of("key").unwrap();
-    if Path::new(key).exists() {
-        // Key is a file, read the contents of the file.
-        File::open(key).unwrap().read_to_end(&mut key_bytes).unwrap();
-    } else {
-        // Key is a string, use the string bytes
-        key_bytes = key.to_string().into_bytes();
-    }
-
-    key_bytes
-}
-
-fn write_encoded_bytes<'a>(matches : &'a ArgMatches<'a>, encoded_bytes : Vec<u8>) {
-    if matches.is_present("output") {
-        let mut output = OpenOptions::new()
-            .write(true)
-            .create(true)
-            .truncate(true)
-            .open(matches.value_of("output").unwrap())
-            .unwrap();
-        let _ = output.write_all(encoded_bytes.as_slice());
-        output.flush().unwrap();
-    } else {
-        match String::from_utf8(encoded_bytes) {
-            Ok(encoded) => println!("{}", encoded),
-            Err(e) => println!("{}\n\nDetails: {}", ERR_ENCODED_DATA_NOT_UTF8, e)
-        }
-    }
-}
-
 fn main() {
 
     // Parse arguments and provide help.
@@ -124,4 +91,37 @@ fn main() {
     }
 
     write_encoded_bytes(&matches, encoded_bytes);
+}
+
+fn get_key_bytes<'a>(matches: &'a ArgMatches<'a>) -> Vec<u8> {
+    let mut key_bytes : Vec<u8> = Vec::new();
+
+    let key = matches.value_of("key").unwrap();
+    if Path::new(key).exists() {
+        // Key is a file, read the contents of the file.
+        File::open(key).unwrap().read_to_end(&mut key_bytes).unwrap();
+    } else {
+        // Key is a string, use the string bytes
+        key_bytes = key.to_string().into_bytes();
+    }
+
+    key_bytes
+}
+
+fn write_encoded_bytes<'a>(matches : &'a ArgMatches<'a>, encoded_bytes : Vec<u8>) {
+    if matches.is_present("output") {
+        let mut output = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(matches.value_of("output").unwrap())
+            .unwrap();
+        let _ = output.write_all(encoded_bytes.as_slice());
+        output.flush().unwrap();
+    } else {
+        match String::from_utf8(encoded_bytes) {
+            Ok(encoded) => println!("{}", encoded),
+            Err(e) => println!("{}\n\nDetails: {}", ERR_ENCODED_DATA_NOT_UTF8, e)
+        }
+    }
 }
