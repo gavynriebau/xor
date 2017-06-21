70c1218c7cb1fe35a4037c47e9aa6790681c927e
gavynriebau <gavyn.riebau@gmail.com>
2017-06-21 17:57:16 +0800

    Partway through recursive encrypting directories

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate xor_utils;
 
 use clap::{App, Arg, ArgMatches};
 use std::io;
+use std::fs;
 use std::path::Path;
 use std::fs::{File, OpenOptions};
 use std::io::{Write, Read};
@@ -18,7 +19,7 @@ fn main() {
 
     // Parse arguments and provide help.
     let matches = App::new("xor")
-        .version("1.2.2")
+        .version("1.3.0")
         .about("XORs input against a provided key")
         .author("Gavyn Riebau")
         .arg(Arg::with_name("key")
@@ -28,11 +29,17 @@ fn main() {
              .required(true)
              .value_name("KEY"))
         .arg(Arg::with_name("input")
-             .help("The file from which input data will be read, if omitted input will be read from stdin")
+             .help("The file / directory from which input data will be read, if omitted input will be read from stdin.\nIf a directory is specified, all files inside the directory will be encryted.")
              .long("input")
              .short("i")
              .required(false)
              .value_name("FILE"))
+        .arg(Arg::with_name("recursive")
+             .help("Recursively encrypt files and subfolders starting at the given directory")
+             .long("recursive")
+             .short("r")
+             .conflicts_with("output")
+             .value_name("DIRECTORY"))
         .arg(Arg::with_name("output")
              .help("The file to which encoded data will be written, if omitted output will be written to stdout.\nIt's recommended to write output to a file for cases where the encoded data contains non-unicode characters.")
              .long("output")
@@ -49,17 +56,59 @@ fn main() {
     let key_bytes = get_key_bytes(&matches);
     let stdin = io::stdin();
 
-    // If the "file" argument was supplied input will be read from the file, otherwise
-    // input is read from stdin.
-    let mut input : Box<Read> = if matches.is_present("input") {
-        Box::new(File::open(matches.value_of("input").unwrap()).unwrap())
+    if matches.is_present("recursive") {
+        // Recursively encrypt files and folders in the specified directory.
+        let starting_dir_name = matches.value_of("recursive").unwrap();
+        let starting_dir = Path::new(starting_dir_name);
+
+        if !starting_dir.is_dir() {
+            panic!("Supplied value for option 'recursive' was not a directory.")
+        }
+
+        recursively_encrypt(starting_dir);
     } else {
-        Box::new(stdin.lock())
-    };
+        // If the "file" argument was supplied input will be read from the file, otherwise
+        // input is read from stdin.
+        let mut input : Box<Read> = if matches.is_present("input") {
+            Box::new(File::open(matches.value_of("input").unwrap()).unwrap())
+        } else {
+            Box::new(stdin.lock())
+        };
 
-    let encoded_bytes = input.by_ref().xor(key_bytes);
+        let encoded_bytes = input.by_ref().xor(key_bytes);
 
-    write_encoded_bytes(&matches, encoded_bytes);
+        write_encoded_bytes(&matches, encoded_bytes);
+    }
+}
+
+fn recursively_encrypt(p : &Path) {
+    if p.is_dir() {
+        println!("Path was a directory");
+        match fs::read_dir(p) {
+            Ok(entries) => {
+                for entry in entries {
+                    if let Ok(entry) = entry {
+                        if let Ok(file_type) = entry.file_type() {
+                            println!("{:?} - {:?}", entry.path(), file_type);
+                            if file_type.is_dir() {
+                                let p = &entry.path();
+                                let path = Path::new(p);
+                                recursively_encrypt(path);
+                            }
+                        }
+                    }
+                }
+            },
+            Err(e) => {
+                let mut stderr = io::stderr();
+                let _ = stderr.write_fmt(format_args!("Failed to read directory: {}", e));
+            }
+        }
+    } else if p.is_file() {
+        println!("Path was a file");
+    } else {
+        println!("Path was something else");
+    }
 }
 
 fn get_key_bytes<'a>(matches: &'a ArgMatches<'a>) -> Vec<u8> {
