1ddf6f9af80be1eb3d7ab2c5acd5999c75489713
Gavyn Riebau <gavyn.riebau@gmail.com>
2017-12-12 17:26:32 +0800

    Partway through swap to use filesystem crate

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,18 @@ extern crate xor_utils;
 extern crate hex;
 extern crate base64;
 extern crate number_prefix;
+extern crate filesystem;
 
 #[macro_use] extern crate log;
 extern crate env_logger;
 
 use clap::{App, Arg, ArgMatches};
-use std::ops::{DerefMut};
 use std::io;
 use std::fs;
 use std::path::Path;
-use std::fs::{File, OpenOptions, DirEntry};
 use std::io::{Write, Read, Cursor};
 use number_prefix::{binary_prefix, Standalone, Prefixed};
+use filesystem::{FileSystem, DirEntry};
 
 /// The mode is used in conjunction with the "recursive" option and determines how file names
 /// will be processed when renaming files.
@@ -95,37 +95,40 @@ fn main() {
     let key_bytes = get_key_bytes(&matches);
 
     if matches.is_present("recursive") {
-        trace!("Recursively encrypting files and folders.");
+        // trace!("Recursively encrypting files and folders.");
 
-        let starting_dir_name = matches.value_of("recursive").unwrap();
-        let starting_dir = Path::new(starting_dir_name);
+        // let starting_dir_name = matches.value_of("recursive").unwrap();
+        // let starting_dir = Path::new(starting_dir_name);
 
-        if mode == Mode::Decrypt || matches.is_present("force") || check_sizes(starting_dir, &key_bytes) {
-            encrypt_path(starting_dir, &key_bytes, &mode);
-        }
+        // if mode == Mode::Decrypt || matches.is_present("force") || check_sizes(starting_dir, &key_bytes) {
+        //     encrypt_path(starting_dir, &key_bytes, &mode);
+        // }
     } else {
 
-        let mut output : Box<Write> = if matches.is_present("output") {
-            trace!("Writting output to a file.");
-            Box::new(OpenOptions::new()
-                .write(true)
-                .create(true)
-                .truncate(true)
-                .open(matches.value_of("output").unwrap())
-                .unwrap())
-        } else {
-            trace!("Writting output to stdout.");
-            Box::new(stdout_writer::StdoutWriter{})
-        };
+        // let mut output : Box<Write> = if matches.is_present("output") {
+        //     trace!("Writting output to a file.");
+
+        //     // let x = filesystem.path();
+
+        //     // Box::new(OpenOptions::new()
+        //     //     .write(true)
+        //     //     .create(true)
+        //     //     .truncate(true)
+        //     //     .open(matches.value_of("output").unwrap())
+        //     //     .unwrap())
+        // } else {
+        //     trace!("Writting output to stdout.");
+        //     Box::new(stdout_writer::StdoutWriter{})
+        // };
 
         if matches.is_present("input") {
-            trace!("Reading input from a file.");
-            let mut file_reader= File::open(matches.value_of("input").unwrap()).unwrap();
-            encrypt_reader(&mut file_reader, &key_bytes, output.deref_mut());
+            // trace!("Reading input from a file.");
+            // let mut file_reader= File::open(matches.value_of("input").unwrap()).unwrap();
+            // encrypt_reader(&mut file_reader, &key_bytes, output.deref_mut());
         } else {
-            trace!("Reading input from stdin.");
-            let mut stdin_reader = io::stdin();
-            encrypt_reader(&mut stdin_reader, &key_bytes, output.deref_mut());
+            // trace!("Reading input from stdin.");
+            // let mut stdin_reader = io::stdin();
+            // encrypt_reader(&mut stdin_reader, &key_bytes, output.deref_mut());
         };
     }
 }
@@ -154,95 +157,92 @@ fn encrypt_reader(input : &mut Read, key : &Vec<u8>, output : &mut Write) {
     }
 }
 
-fn encrypt_path(p : &Path, key : &Vec<u8>, mode : &Mode) {
-    for item in fs::read_dir(p).unwrap() {
-        match item {
-            Ok(entry) => xor_entry(&entry, key, mode),
-            Err(err) => info!("Failed to read entry because: {}", err)
-        }
-    }
+//fn encrypt_path(p : &VPath, key : &Vec<u8>, mode : &Mode) {
+    // for item in fs::read_dir(p).unwrap() {
+    //     match item {
+    //         Ok(entry) => xor_entry(&entry, key, mode),
+    //         Err(err) => info!("Failed to read entry because: {}", err)
+    //     }
+    // }
+//}
+
+//fn xor_entry(entry : &VPath, key : &Vec<u8>, mode : &Mode) {
+    // match entry.file_type() {
+    //     Ok(entry_type) => {
+    //         if entry_type.is_dir() {
+    //             xor_dir(entry, key, mode);
+    //         } else if entry_type.is_file() {
+    //             xor_file(entry, key, mode);
+    //         } else if entry_type.is_symlink() {
+    //             xor_symlink(entry, key, mode);
+    //         }
+    //     },
+    //     Err(err) => info!("Failed to get filetype for DirEntry {:?} because: {}", entry, err)
+    // }
+//}
+
+fn xor_file<T : FileSystem>(fs : &T, file_path : &Path, key : &Vec<u8>, mode : &Mode) {
+    debug!("Encrypting file {:?}", file_path);
+
+    // match file_path.metadata() {
+    //     Ok(metadata) => {
+    //         let mut buffer : Cursor<Vec<u8>> = Cursor::new(Vec::with_capacity(metadata.len() as usize));
+
+    //         encrypt_reader(&mut file, &key, &mut buffer);
+
+    //         let output_options = OpenOptions::new()
+    //             .create(true)
+    //             .write(true)
+    //             .truncate(true);
+
+    //         match file_path.open_with_options(output_options) {
+    //             Ok(mut writer) => {
+    //                 let _ = writer.write_all(buffer.get_mut());
+    //                 //let cloned_buffer = raw_buffer.clone();
+    //                 //std::fs::rename(temp_file_path, entry.path()).unwrap();
+    //             },
+    //             Err(err) => info!("Failed to open file with truncate option for DirEntry {:?} because: {}", entry.path(), err)
+    //         }
+    //     },
+    //     Err(err) => info!("Failed to read metadata of file at path {:?} because: {}", file_path, err)
+    // }
+
+   rename_entry(fs, file_path, key, mode);
 }
 
-fn xor_entry(entry : &DirEntry, key : &Vec<u8>, mode : &Mode) {
-    match entry.file_type() {
-        Ok(entry_type) => {
-            if entry_type.is_dir() {
-                xor_dir(entry, key, mode);
-            } else if entry_type.is_file() {
-                xor_file(entry, key, mode);
-            } else if entry_type.is_symlink() {
-                xor_symlink(entry, key, mode);
-            }
-        },
-        Err(err) => info!("Failed to get filetype for DirEntry {:?} because: {}", entry, err)
-    }
-}
+// fn xor_symlink(entry : &DirEntry, key : &Vec<u8>, mode : &Mode) {
+//     debug!("Encrypting symlink {:?}", entry);
 
-fn xor_file(entry : &DirEntry, key : &Vec<u8>, mode : &Mode) {
-    debug!("Encrypting file {:?}", entry);
+//     rename_entry(entry, key, mode);
+// }
 
-    match File::open(entry.path()) {
-        Ok(mut file) => {
+// fn xor_dir(entry : &DirEntry, key : &Vec<u8>, mode : &Mode) {
+//     debug!("Encrypting dir {:?}", entry);
 
-            let meta_data = file.metadata().unwrap();
-            let mut buffer : Cursor<Vec<u8>> = Cursor::new(Vec::with_capacity(meta_data.len() as usize));
+//     match fs::read_dir(entry.path()) {
+//         Ok(entries) => {
+//             for child in entries {
+//                 if let Ok(child) = child {
+//                     xor_entry(&child, key, mode);
+//                 }
+//             }
+//         },
+//         Err(e) => {
+//             let mut stderr = io::stderr();
+//             let _ = stderr.write_fmt(format_args!("Failed to read directory: {}", e));
+//         }
+//     }
 
-            encrypt_reader(&mut file, &key, &mut buffer);
-
-            let output = OpenOptions::new()
-                .create(true)
-                .write(true)
-                .truncate(true)
-                .open(entry.path());
-
-            match output {
-                Ok(mut writer) => {
-                    let _ = writer.write_all(buffer.get_mut());
-                    //let cloned_buffer = raw_buffer.clone();
-                    //std::fs::rename(temp_file_path, entry.path()).unwrap();
-                },
-                Err(err) => info!("Failed to open file with truncate option for DirEntry {:?} because: {}", entry.path(), err)
-            }
-        },
-        Err(err) => info!("Failed to open file for DirEntry {:?} because: {}", entry, err)
-    }
-
-    rename_entry(entry, key, mode);
-}
-
-fn xor_symlink(entry : &DirEntry, key : &Vec<u8>, mode : &Mode) {
-    debug!("Encrypting symlink {:?}", entry);
-
-    rename_entry(entry, key, mode);
-}
-
-fn xor_dir(entry : &DirEntry, key : &Vec<u8>, mode : &Mode) {
-    debug!("Encrypting dir {:?}", entry);
-
-    match fs::read_dir(entry.path()) {
-        Ok(entries) => {
-            for child in entries {
-                if let Ok(child) = child {
-                    xor_entry(&child, key, mode);
-                }
-            }
-        },
-        Err(e) => {
-            let mut stderr = io::stderr();
-            let _ = stderr.write_fmt(format_args!("Failed to read directory: {}", e));
-        }
-    }
-
-    rename_entry(entry, key, mode);
-}
+//     rename_entry(entry, key, mode);
+// }
 
 /// Renames a directory entry.
 /// When "mode" is Mode::Encrypt, the name of the entry is XOR'd then hexlified.
 /// When "mode" is Mode::Decrypt, the name of the entry is unhexlified then XOR'd.
-fn rename_entry(entry : &DirEntry, key : &Vec<u8>, mode : &Mode) {
-    let file_name = entry.file_name();
+fn rename_entry<T : FileSystem>(fs : &T, entry : &Path, key : &Vec<u8>, mode : &Mode) {
 
-    if let Some(original_name) = file_name.to_str() {
+    if let Some(original_name_osstr) = entry.file_name() {
+        let original_name = String::from(original_name_osstr.to_str().unwrap());
         debug!("original_name: {}", original_name);
 
         let key_repeated = repeat_key(key, original_name.len());
@@ -250,8 +250,8 @@ fn rename_entry(entry : &DirEntry, key : &Vec<u8>, mode : &Mode) {
         // If in Encrypt mode use the filename as is.
         // If in Decrypt mode unhexify the filename before getting it's bytes.
         let input_bytes = match *mode {
-            Mode::Encrypt => original_name.to_string().into_bytes(),
-            Mode::Decrypt => from_hex_string(String::from(original_name))
+            Mode::Encrypt => original_name.clone().into_bytes(),
+            Mode::Decrypt => from_hex_string(&original_name)
         };
 
         // Xor encrypt the name.
@@ -268,22 +268,13 @@ fn rename_entry(entry : &DirEntry, key : &Vec<u8>, mode : &Mode) {
         };
         debug!("replaced_name: {}", replaced_name);
 
-        let full_path_buf = entry.path();
-        let full_path = full_path_buf.as_path();
-        let parent_path = full_path.parent().unwrap();
-
-        let src_file_path_buf = parent_path.join(original_name);
-        let dst_file_path_buf = parent_path.join(replaced_name);
-
-        let src_file_path = src_file_path_buf.as_path();
-        let dst_file_path = dst_file_path_buf.as_path();
+        let parent_path = entry.parent().unwrap();
+        let src_file_path = parent_path.join(&original_name);
+        let dst_file_path = parent_path.join(&replaced_name);
 
         debug!("Moving {:?} to {:?}", src_file_path, dst_file_path);
 
-        match fs::rename(src_file_path, dst_file_path) {
-            Ok(_) => {},
-            Err(e) => info!("Failed to rename file because: {}", e)
-        }
+        fs.rename(src_file_path, dst_file_path).unwrap();
     }
 }
 
@@ -314,100 +305,105 @@ fn to_hex_string(bytes: Vec<u8>) -> String {
     strings.join("")
 }
 
-fn from_hex_string(hex : String) -> Vec<u8> {
+fn from_hex_string(hex : &String) -> Vec<u8> {
     hex::FromHex::from_hex(hex).unwrap()
 }
 
 fn get_key_bytes<'a>(matches: &'a ArgMatches<'a>) -> Vec<u8> {
     let mut key_bytes : Vec<u8> = Vec::new();
 
-    let key = matches.value_of("key").unwrap();
+    // let key = matches.value_of("key").unwrap();
 
-    // If the key is a file, read the contents of the file.
-    // Otherwise if key is a string, use the string bytes.
-    if Path::new(key).exists() {
-        File::open(key).unwrap().read_to_end(&mut key_bytes).unwrap();
-    } else {
-        key_bytes = key.to_string().into_bytes();
-    }
+    // // If the key is a file, read the contents of the file.
+    // // Otherwise if key is a string, use the string bytes.
+    // if Path::new(key).exists() {
+    //     File::open(key).unwrap().read_to_end(&mut key_bytes).unwrap();
+    // } else {
+    //     key_bytes = key.to_string().into_bytes();
+    // }
 
     key_bytes
 }
 
 /// Recursively searches the supplied path and finds the size of the largest file.
-fn get_largest_file_size(path : &Path) -> u64 {
-    let mut size : u64 = 0;
+//fn get_largest_file_size(path : &VPath) -> u64 {
+    // let mut size : u64 = 0;
 
-    // Check if the current file is the largest.
-    if path.is_file() {
-        match path.metadata() {
-            Ok(metadata) => {
-                size = metadata.len();
-            },
-            Err(_) => {
-                size = 0;
-            }
-        }
-    } else if path.is_dir() {
-        // Check if any of the child files are the largest.
-        for entry_result in fs::read_dir(path).unwrap() {
-            if let Ok(entry) = entry_result {
-                let entry_size = get_largest_file_size(entry.path().as_path());
+    // match path.metadata() {
+    //     Ok(metadata) => {
+            
+    //         // Check if the current file is the largest.
+    //         if path.is_file() {
+    //             size = metadata.len();
+    //         } else if path.is_dir() {
+    //             // Check if any of the child files are the largest.
+    //             match path.read_dir() {
+    //                 Ok(entries) => {
+    //                     for entry in entries {
+    //                         let entry_size = get_largest_file_size(entry.path().as_path());
 
-                if entry_size > size {
-                    size = entry_size;
-                }
-            }
-        }
-    }
+    //                         if entry_size > size {
+    //                             size = entry_size;
+    //                         }
+    //                     }
+    //                 },
+    //                 Err(err) => info!("Failed to read directory {:?} because: {}", path, err)
+    //             }
+    //         }
+    //     },
+    //     Err => info!("Failed to get metadata for path {:?} because: {}", path, err)
+    // }
 
-    size
-}
+    // size
+    //0
+//}
 
 /// Recursively searches the supplied path and finds the length of the longest file/directory name.
-fn get_longest_name(path : &Path) -> usize {
-    let mut size : usize = 0;
+//fn get_longest_name(path : &VPath) -> usize {
+    // let mut size : usize = 0;
 
-    // Check if the current entry name is the longest.
-    if let Some(name) = path.file_name() {
-        let length = name.len();
+    // // Check if the current entry name is the longest.
+    // if let Some(name) = path.file_name() {
+    //     let length = name.len();
 
-        if length > size {
-            size = length;
-        }
+    //     if length > size {
+    //         size = length;
+    //     }
 
-        if path.is_dir() {
-            // Check if any of the child directory / file names are the longest.
-            for entry_result in fs::read_dir(path).unwrap() {
-                if let Ok(entry) = entry_result {
-                    let entry_size = get_longest_name(entry.path().as_path());
+    //     if path.is_dir() {
+    //         // Check if any of the child directory / file names are the longest.
+    //         for entry_result in fs::read_dir(path).unwrap() {
+    //             if let Ok(entry) = entry_result {
+    //                 let entry_size = get_longest_name(entry.path().as_path());
 
-                    if entry_size > size {
-                        size = entry_size;
-                    }
-                }
-            }
-        }
-    }
+    //                 if entry_size > size {
+    //                     size = entry_size;
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
 
-    size
-}
+    // size
 
-fn check_sizes(starting_directory : &Path, key_bytes : &Vec<u8>) -> bool {
-    let mut should_continue : bool = true;
+    //0
+//}
 
-    let key_size = key_bytes.len();
-    let largest_file_size = get_largest_file_size(starting_directory);
-    let longest_name = get_longest_name(starting_directory);
+// fn check_sizes(starting_directory : &Path, key_bytes : &Vec<u8>) -> bool {
+//     let mut should_continue : bool = true;
 
-    if largest_file_size > key_size as u64 || longest_name > key_size {
-        print_keysize_warning(key_size, largest_file_size, longest_name);
-        let answer = show_prompt();
-        should_continue = answer == 'y';
-    }
+//     let key_size = key_bytes.len();
+//     let largest_file_size = get_largest_file_size(starting_directory);
+//     let longest_name = get_longest_name(starting_directory);
 
-    should_continue
-}
+//     if largest_file_size > key_size as u64 || longest_name > key_size {
+//         print_keysize_warning(key_size, largest_file_size, longest_name);
+//         let answer = show_prompt();
+//         should_continue = answer == 'y';
+//     }
+
+//     should_continue
+// }
 
 fn show_prompt() -> char {
     let mut answer : char = '_';
@@ -462,6 +458,7 @@ Sizes:");
 #[cfg(test)]
 mod tests {
     use super::*;
+    use filesystem::*;
 
     #[test]
     fn to_hex_string_works() {
@@ -475,7 +472,7 @@ mod tests {
     #[test]
     fn from_hex_string_works() {
         let input_string = String::from("68656C6C6F");
-        let ascii_bytes = from_hex_string(input_string);
+        let ascii_bytes = from_hex_string(&input_string);
         let expected_bytes = vec![104, 101, 108, 108, 111];
 
         assert_eq!(expected_bytes, ascii_bytes);
@@ -495,6 +492,45 @@ mod tests {
         let cipher_text = String::from_utf8(writer.into_inner()).unwrap();
 
         assert_eq!(expected, cipher_text);
+    }
+
+    // Gets a list of files in the current directory of the given FileSystem type
+    fn get_root_files<T : FileSystem>(fs : &T) -> Vec<String> {
+        let mut filenames = Vec::new();
+
+        let root = fs.current_dir().unwrap();
+        let entries = fs.read_dir(root).unwrap();
+        for entry in entries {
+            if let Ok(dir) = entry {
+                filenames.push(String::from(dir.path().into_os_string().to_str().unwrap()));
+            }
+        }
+
+        filenames
+    }
+
+    #[test]
+    fn xor_file_works() {
+        // Arrange.
+
+        // Setup the input file
+        let fs = FakeFileSystem::new();
+        let root = fs.current_dir().unwrap();
+        let input_path = root.join("input.txt");
+        let input_data = "hello world".as_bytes();
+        fs.create_file(&input_path, input_data).unwrap();
+
+        let key = vec![71];
+        let mode = Mode::Encrypt;
+
+        // Act.
+        xor_file(&fs, &input_path, &key, &mode);
+
+        // Assert.
+        let mut filenames = get_root_files(&fs);
+
+        // Filename is XOR'd against the key then encoded to hex
+        assert_eq!(filenames, vec!["/2E2937323369333F33"]);
     }
 
 }
